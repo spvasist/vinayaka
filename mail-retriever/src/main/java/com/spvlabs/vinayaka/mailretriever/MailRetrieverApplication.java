ec48744a31e80440978ca2a4ac90aa6c5ac24b4c
Srikanth P Vasist <spvasista@gmail.com>
2021-01-01 00:46:48 +0530

    Initial code

---
+++ mail-retriever/src/main/java/com/spvlabs/vinayaka/mailretriever/MailRetrieverApplication.java
@@ -0,0 +1,13 @@
+package com.spvlabs.vinayaka.mailretriever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailRetrieverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailRetrieverApplication.class, args);
+    }
+
+}
