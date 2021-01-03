ea64a45b63ec838dc17eaa17a96566c12e8fdc40
Srikanth P Vasist <spvasist@users.noreply.github.com>
2021-01-03 23:08:26 +0530

    Added categorizer code base (#2)

---
+++ mail-categorizer/src/main/java/com/spvlabs/mailcategorizer/MailCategorizerApplication.java
@@ -0,0 +1,13 @@
+package com.spvlabs.mailcategorizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailCategorizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailCategorizerApplication.class, args);
+    }
+
+}
